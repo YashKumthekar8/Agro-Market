a26760eee9231d70eaf78c8c8e78b9a51b9f15dc
yash <yashkumthekar1@gmail.com>
2021-09-26 00:37:03 +0530

    Updated DataBase. Added Plant ID to User For Recommending Product. Added Address to the Seller

--- AgroHostile/selling_app/models.py
+++ AgroHostile/selling_app/models.py
@@ -1,19 +1,11 @@
-from django.db.models.deletion import Collector
+from django.db.models.deletion import CASCADE, Collector
 from djongo import models
 
 
 class Seller(models.Model):
     name = models.CharField(max_length=30)
     phone = models.IntegerField()
-    # flower_id = models.ForeignKey(to=)
-
-    def __str__(self):
-        return self.name
-
-class User(models.Model):
-    name = models.CharField(max_length=30)
-    gmail = models.CharField(max_length=50)
-    phone = models.IntegerField()
+    address = models.CharField(max_length=150, default=None)
 
     def __str__(self):
         return self.name
@@ -31,9 +23,12 @@ class Plant(models.Model):
     height = models.CharField(max_length=10)
     spacing = models.CharField(max_length=10)
     color = models.CharField(max_length=10)
-    season = models.CharField(max_length=30)
-    soil_requirements = models.CharField(max_length=30)
-    growth_rate = models.CharField(max_length=10)
+    season = models.CharField(max_length=40)
+    soil_requirements = models.CharField(max_length=50)
+    growth_rate = models.CharField(max_length=40)
+    location = models.ForeignKey(Location, default=None, on_delete=models.CASCADE)
+    seller = models.ManyToManyField(Seller, default=None)
+
 
     def __str__(self):
         return self.name
@@ -44,6 +39,18 @@ class Fertilizer(models.Model):
     description = models.CharField(max_length=100)
     weight = models.CharField(max_length=10)
     method_to_use = models.CharField(max_length=100)
+    location = models.ForeignKey(Location, default=None, on_delete=models.CASCADE)
+    seller = models.ManyToManyField(Seller, default=None)
     
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    gmail = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    plant = models.OneToOneField(Plant, on_delete=models.CASCADE, primary_key=True)
+
     def __str__(self):
         return self.name
